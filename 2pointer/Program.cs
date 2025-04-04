e007bfc1d8bbda789555f5c2b596efca478e1ab5
priya lall <lall.priya89@gmail.com>
2025-04-04 11:28:36 +0530

    Add project files.

---
+++ 2pointer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace LearnDictionaryTutorial
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] arr1 = { 1, 3, 5 };
+            int[] arr2 = { 2,4,6 };
+
+            int[] result = new int[arr1.Length + arr2.Length];
+            int i = 0, j = 0, k = 0;
+
+            while (i < arr1.Length && j < arr2.Length)
+            {
+                if (arr1[i] < arr2[j])
+                {
+                    result[k++] = arr1[i++];
+                    
+                }
+                else
+                {
+                    result[k++] = arr2[j++];
+                }
+
+
+            }
+
+
+
+            for(int m =0; m < result.Length; m++)
+            Console.WriteLine(result[m]);
+        }
+    }
+}
---
+++ LearnDictionarytutorial1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace LearnDictionaryTutorial
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Dictionary<int, string> students = new Dictionary<int, string>();
+            students.Add(102, "Amara");
+            students.Add(103, "Rahul");
+            students.Add(104, "Kia");
+            Console.WriteLine( "Student List :");
+
+            foreach (var student in students) 
+            {
+                Console.WriteLine($"ID: {student.Key} ,Name :{student.Value}");
+            }
+
+
+
+
+        }
+    }
+}
